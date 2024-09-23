52cd560fc8f6165d319e611bcafe2ed069ca56b5
living.xu <249331051@qq.com>
2024-09-23 19:50:56 +0800

    first commit

---
+++ src/main/java/base/bytebuffer/TestByteBufferRead.java
@@ -0,0 +1,29 @@
+package base.bytebuffer;
+
+import java.nio.ByteBuffer;
+
+import static base.bytebuffer.ByteBufferUtil.debugAll;
+
+public class TestByteBufferRead {
+    public static void main(String[] args) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+        byteBuffer.put(new byte[]{'a','b','c','d'});
+        byteBuffer.flip();
+
+//        byteBuffer.get(new byte[4]);
+//        debugAll(byteBuffer);
+//        byteBuffer.rewind();
+//        System.out.println((char)byteBuffer.get());
+/*        System.out.println((char)byteBuffer.get());
+        System.out.println((char)byteBuffer.get());
+        byteBuffer.mark();
+        System.out.println((char)byteBuffer.get());
+        System.out.println((char)byteBuffer.get());
+//        byteBuffer.reset();
+        byteBuffer.rewind();
+        System.out.println((char)byteBuffer.get());
+        System.out.println((char)byteBuffer.get());*/
+        System.out.println((char)byteBuffer.get(3));
+        debugAll(byteBuffer);
+    }
+}
