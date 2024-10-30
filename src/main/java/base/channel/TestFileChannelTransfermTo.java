241a557be60498cab0576eba713acda487338b9c
living.xu <249331051@qq.com>
2024-10-30 20:27:24 +0800

    ignore

---
+++ src/main/java/base/channel/TestFileChannelTransfermTo.java
@@ -0,0 +1,19 @@
+package base.channel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+public class TestFileChannelTransfermTo {
+    public static void main(String[] args) {
+        try {
+            FileChannel form = new FileInputStream("data.txt").getChannel();
+            FileChannel to = new FileOutputStream("to.txt").getChannel();
+            //效率高，底层会使用零拷贝进行优化，传输上限只有2G
+            form.transferTo(0,form.size(),to);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/base/channel/TestFileChannelTransfermTo2.java
@@ -0,0 +1,23 @@
+package base.channel;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+public class TestFileChannelTransfermTo2 {
+    public static void main(String[] args) {
+        try {
+            FileChannel form = new FileInputStream("data.txt").getChannel();
+            FileChannel to = new FileOutputStream("to.txt").getChannel();
+            //效率高，底层会使用零拷贝进行优化，传输上限只有2G
+            long size = form.size();
+            //这样可以分多次传输大于2G的文件
+            for (long left = size; left > 0; ) {
+                left -= form.transferTo((size - left), left, to);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
