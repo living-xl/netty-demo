4e9d2b2bb1c0e2dbabcd430545c4363b3f21e562
living.xu <249331051@qq.com>
2024-12-07 17:26:28 +0800

    2024年12月7日17:26:10 eventloop细化

---
+++ src/main/java/base/netty/eventloop/EventLoopServer2.java
@@ -0,0 +1,44 @@
+package base.netty.eventloop;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.DefaultEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+public class EventLoopServer2 {
+    public static void main(String[] args) {
+        DefaultEventLoopGroup defaultEventLoopGroup = new DefaultEventLoopGroup(2);
+        new ServerBootstrap()
+                .group(new NioEventLoopGroup(),new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast("handler1",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug("revice msg -{}",buf.toString(Charset.defaultCharset()));
+                                ctx.fireChannelRead(msg);
+                            }
+                        }).addLast(defaultEventLoopGroup,"handler2",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug("revice msg -{}",buf.toString(Charset.defaultCharset()));
+                            }
+                        });
+                    }
+                })
+                .bind(9999);
+    }
+}
